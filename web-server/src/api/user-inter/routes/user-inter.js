7d18a0b914a47ee63c33413b7947603d0971a125
Ilham Hajidoloh <ihsaniah112@gmail.com>
2025-07-25 18:19:26 +0700

    update

---
+++ web-server/src/api/user-inter/routes/user-inter.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-inter router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-inter.user-inter');
--- web-server/types/generated/contentTypes.d.ts
+++ web-server/types/generated/contentTypes.d.ts
@@ -373,6 +373,36 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiUserInterUserInter extends Struct.CollectionTypeSchema {
+  collectionName: 'user_inters';
+  info: {
+    displayName: 'user_inter';
+    pluralName: 'user-inters';
+    singularName: 'user-inter';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    full_name: Schema.Attribute.Text & Schema.Attribute.Required;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::user-inter.user-inter'
+    > &
+      Schema.Attribute.Private;
+    password: Schema.Attribute.Password;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    usernamde: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -882,6 +912,7 @@ declare module '@strapi/strapi' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
+      'api::user-inter.user-inter': ApiUserInterUserInter;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
