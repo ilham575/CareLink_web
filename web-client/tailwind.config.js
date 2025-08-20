270daa1b2c9da7bfb5abe0e81c45c86b2637d1af
อิลฮัม หะยีดอเล๊าะ <ihsaniah112@gmail.com>
2025-08-20 22:29:05 +0700

    Add tailwind

---
+++ web-client/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",     // สำหรับ React
+    "./public/index.html"             // เพิ่มกรณีใช้ HTML ตรง
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
